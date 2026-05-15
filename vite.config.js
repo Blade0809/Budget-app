5cb05a52f24fd21fb67c21ee3c22421a09f24c2f
root <liuliu154531@icloud.com>
2026-05-15 17:27:17 +0000

    Generate lcov coverage report for Codecov

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       include: ["src/**/*.js"],
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "lcov"],
       thresholds: {
         statements: 80,
         branches: 80,
